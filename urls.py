3ab6ea8154bdef66a8d3d70902843d3067e1d494
chatra k <chatra@MacBook-Air-3.local>
2019-05-20 20:36:36 +0530

    missed cap in url

--- urls.py
+++ urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^unlike', views.unlike, name="blog unliked"),
     url(r'^comment', views.comment, name="commented on blog"),
     url(r'^list_blogs', views.list_blogs, name="List all the blogs"),
-    url(r'blog_info', views.blog_info, name="get blog info")
+    url(r'^blog_info', views.blog_info, name="get blog info")
 ]
